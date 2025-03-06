d81a22bcd4f48298b5ed911ab1f46c28e53241b0
shotapentaho <sobhan.hota@hitachivantara.com>
2025-03-06 17:26:56 -0500

    more imports

--- st_langchain.py
+++ st_langchain.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import openai
+import langchain
 from langchain.llms import OpenAI
+from langchain_community import llms
+from langchain.chains import LLMChain
+from langchain_openai import OpenAI
 
 st.title("🦜🔗 Langchain Sobhan's first app")
 
