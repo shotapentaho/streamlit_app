ddf31cd7225a50fd2b6de813e37a99decd10653b
shotapentaho <sobhan.hota@hitachivantara.com>
2025-03-15 19:44:44 -0400

    updated

--- st_summarize_text.py
+++ st_summarize_text.py
@@ -5,7 +5,7 @@ from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
 
 # Ensure punkt is downloaded
-nltk.download("punkt")
+nltk.download('punkt')
 
 # Function to summarize text
 def summarize_text(text, num_sentences):
