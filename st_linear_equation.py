f2b46dab2e506a15f6ce5b4b1bf757c41cbb61a9
shotapentaho <sobhan.hota@hitachivantara.com>
2025-03-16 16:56:46 -0400

    title

--- st_linear_equation.py
+++ st_linear_equation.py
@@ -3,11 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Set page config to full-width layout
-st.set_page_config(page_title="📈 Linear Equation Solver & Grapher : y = ax + b", layout="wide")
+st.title("📈 Linear Equation Solver & Grapher : y = ax + b")
 
 # Split the screen into two columns
 col1, col2 = st.columns([0.5, 0.5])  # 50-50 split
-
 
 with col1:
 
