832752cb378c0618c3a109b6b4aa6603d4bb96da
shotapentaho <sobhan.hota@hitachivantara.com>
2025-03-16 16:48:02 -0400

    updated

--- st_linear_equation.py
+++ st_linear_equation.py
@@ -2,13 +2,11 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Streamlit UI
+st.set_page_config(page_title="📈 Linear Equation Solver & Grapher : y = ax + b", layout="wide")
 # Split the screen into two columns
 col1, col2 = st.columns([0.5, 0.5])  # 50-50 split
 
-
-# Streamlit UI
-st.set_page_config(page_title="📈 Linear Equation Solver & Grapher : y = ax + b", layout="wide")
 
 with col1:
     # User Inputs with Sliders
