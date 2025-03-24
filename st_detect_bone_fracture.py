bfe842edf34f50cd4a58dea96033fa08e305a597
shotapentaho <sobhan.hota@hitachivantara.com>
2025-03-24 09:44:39 -0400

    build DL model and save

--- st_detect_bone_fracture.py
+++ st_detect_bone_fracture.py
@@ -2,7 +2,23 @@ import streamlit as st
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from PIL import Image
+
+
+# Simple CNN Model
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(224, 224, 3)),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(2, activation='softmax')  # 2 classes: Fracture vs Normal
+])
+
+# Compile Model, Save Model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.save("bone_fracture_model.h5")
 
 # Load the pre-trained model (replace with your model)
 @st.cache_resource
