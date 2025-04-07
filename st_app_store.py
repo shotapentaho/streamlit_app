745ffe20f069e6eb494aefd1dfdd41ec8ae0c4e6
shotapentaho <sobhan.hota@hitachivantara.com>
2025-04-07 19:53:08 -0400

    log curves

--- st_app_store.py
+++ st_app_store.py
@@ -11,6 +11,7 @@ streamlit_urls = {
 	"Quadratic Equation": "https://graphquadratic.streamlit.app/",
     "Area of Sector (Circle)":"https://circle-sector.streamlit.app/",
     "Triangle by co-ordinates": "https://viz-triangle.streamlit.app/",
+    "Multi Log Curves": "https://viz-logarithm.streamlit.app/",
     "Text Summarizer": "https://summarize-info.streamlit.app/",
     "PDF Text Extractor":"https://pdf-to-txt.streamlit.app/",
     "Decision Tree Classifier": "https://hot-decision-tree.streamlit.app/",
