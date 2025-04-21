a03443d0d2fb8c8bb5c64c91ef1ab5babe55c01d
Sobhan Hota <gupuldw@yahoo.com>
2025-04-21 17:32:21 -0400

    removed blank line

--- st_app_store.py
+++ st_app_store.py
@@ -28,7 +28,6 @@ streamlit_urls = {
     "Named Entity Recognition from text":"https://ner-from-text.streamlit.app/",
     "Self Organizing Map - Clustering":"https://self-org-map.streamlit.app/",
     "Game Practice Reminder(SMS- Only US/CA)":"https://game-practice-reminder.streamlit.app/"
-   
 }
 
 # Dropdown or radio button for selection
