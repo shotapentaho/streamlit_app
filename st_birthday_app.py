341a3571f8a2683dc8781c12cf27373e99a36425
Sobhan Hota <gupuldw@yahoo.com>
2025-04-22 18:23:37 -0400

    symbol

--- st_birthday_app.py
+++ st_birthday_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import duckdb
 from datetime import datetime
 
-st.set_page_config(page_title="👨‍👩‍👧‍👦 Family-fun bdays",layout="wide")
+st.set_page_config(page_title="👨‍👩‍👧‍👦🎂 Family-fun bdays",layout="wide")
 
 DB_FILE = "birthdays.duckdb"
 if "con" not in st.session_state:
