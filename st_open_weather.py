12d5aed78bed3184ae9574e2b3149b4b161d0321
Sobhan Hota <gupuldw@yahoo.com>
2025-04-29 19:08:55 -0400

    updated with map

--- st_open_weather.py
+++ st_open_weather.py
@@ -74,6 +74,13 @@ if st.button("5-Day Forecast") and city:
     # Check if the data is valid before displaying
     if data:
         if data.get("cod") == "200":
+            # Show map with city location
+            coord = data['city']['coord']
+            st.map(data=pd.DataFrame([{
+                'lat': coord['lat'],
+                'lon': coord['lon']
+            }]))
+            
             display_forecast(data)
         else:
             st.error("City not found. Please check the name.")
