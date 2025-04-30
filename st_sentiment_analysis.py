ae8b7f90c1eaa3b0cafc67880beddb3e84dec596
Sobhan Hota <gupuldw@yahoo.com>
2025-04-30 19:09:22 -0400

    double quotes

--- st_sentiment_analysis.py
+++ st_sentiment_analysis.py
@@ -3,7 +3,7 @@ from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassifica
 
 # Load tokenizer and model manually
 #model_name = "cardiffnlp/twitter-roberta-base-sentiment"
-model_name = distilbert-base-uncased-finetuned-sst-2-english
+model_name = "distilbert-base-uncased-finetuned-sst-2-english"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
