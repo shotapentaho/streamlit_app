29d012961b427000b77641dd102a052154aec060
Sobhan Hota <gupuldw@yahoo.com>
2025-05-05 19:58:54 -0400

    typo

--- st_app_store.py
+++ st_app_store.py
@@ -20,7 +20,7 @@ st_generic_urls = {
     "Triangle by co-ordinates": "https://viz-triangle.streamlit.app/",
     "Multi Log Curves": "https://viz-logarithm.streamlit.app/",
     "📐Trigonometry Visualizer":"https://trigonometry.streamlit.app/",
-    "HL7-Health Level Seven to CSV Parser" : "https://parse-hl7-emr.streamlit.app/"
+    "HL7-Health Level Seven to CSV Parser":"https://parse-hl7-emr.streamlit.app/"
 
 }
 
