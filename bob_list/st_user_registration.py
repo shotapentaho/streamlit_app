fdc4875fa7b29d0673ad116d53648e9139d6d7bc
Sobhan Hota <gupuldw@yahoo.com>
2025-05-06 19:08:32 -0400

    updated

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -84,6 +84,7 @@ if tab == "Login":
                 #st.write(URL_TO_GO)
                 col_2, col_3 = st.columns([1,2])
                 with col_2:
+                    st.success()
                     #st.success(f"Hello {username}, you're validated!")
                 with col_3:
                     st.markdown(f"""
