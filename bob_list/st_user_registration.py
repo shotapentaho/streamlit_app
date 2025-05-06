94b85d355e2c9886f83946ef295a5f41e73c6ce0
Sobhan Hota <gupuldw@yahoo.com>
2025-05-06 17:13:59 -0400

    updated

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -78,10 +78,10 @@ if tab == "Login":
                 st.session_state.logged_in = True
                 #st.session_state.full_name = full_name
                 st.success(f"Welcome, {username}!")
-                URL_TO_GO = "https://bobs-list.streamlit.app?logged_in=true&username="+ username
+                URL_TO_GO = f"https://bobs-list.streamlit.app?logged_in=true&username={username}"
                 st.write(URL_TO_GO)
                 st.markdown("""
-                <a href=""" + URL_TO_GO + """ target="_blank">
+                <a href="{URL_TO_GO}" target="_blank">
                     <button style='font-size:20px;padding:10px 20px;margin-top:20px;'>Click: Bob's List</button>
                 </a>
                 """, unsafe_allow_html=True)
