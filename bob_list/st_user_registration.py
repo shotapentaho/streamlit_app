ead3a80aca596b2e5567739fe8dba33865ec8efb
Sobhan Hota <gupuldw@yahoo.com>
2025-05-06 16:06:23 -0400

    updafte

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -74,7 +74,7 @@ if tab == "Login":
             #st.experimental_rerun()
             st.markdown("""
             <a href="https://bobs-list.streamlit.app" target="_blank">
-                <button style='font-size:20px;padding:10px 20px;margin-top:20px;'>Go to Bob's List</button>
+                <button style='font-size:20px;padding:10px 20px;margin-top:20px;'>Click: Bob's List</button>
             </a>
             """, unsafe_allow_html=True)
             
