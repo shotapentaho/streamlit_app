a01a6e3edbd677b22b62d4eb60ab489a06afc553
Sobhan Hota <gupuldw@yahoo.com>
2025-05-06 08:03:01 -0400

    updated

--- bob_list/st_contractor_feedback.py
+++ bob_list/st_contractor_feedback.py
@@ -95,11 +95,10 @@ with st.form("engagement_form"):
         else:
             insert_sql = """
                 INSERT INTO TEST.PUBLIC.engagements (
-                    selected_contractor['contractor_id']
-                    customer_name, street, city, state, zip_code, 
+                    contractor_id, customer_name, street, city, state, zip_code, 
                     engagement_type, activity_date, feedback
                 )
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
             """
             cur.execute(insert_sql, (
                 selected_contractor['contractor_id'], customer_name, street, city, state, zip_code,
