ab64f546761128bca9a73cb60e44b7f4a22fd1d4
Sobhan Hota <gupuldw@yahoo.com>
2025-05-07 09:35:52 -0400

    create connection validate

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -39,6 +39,8 @@ def register_user(username, password, full_name):
 
 # Check login
 def authenticate_user(username, password):
+    conn = get_connection()
+    cur = conn.cursor()
     cur.execute("SELECT hashed_password, full_name FROM TEST.PUBLIC.users WHERE username = %s", (username,))
     row = cur.fetchone()
     if not row:
