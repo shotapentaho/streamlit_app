1389a376e9da516896eac7882a6e8b9e57b5ab22
Sobhan Hota <gupuldw@yahoo.com>
2025-05-06 22:02:48 -0400

    update

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -83,6 +83,11 @@ if tab == "Login":
         if st.button("Login"):
             valid, full_name = authenticate_user(username, password)
             if valid:
+                st.markdown(f"""
+                    <a href="{URL_TO_GO}" target="_target">
+                        <button style='font-size:30px;padding:10px 20px;margin-top:20px;'>Click here customer feeback  </button>
+                    </a>
+                    """, unsafe_allow_html=True)
                 st.session_state.logged_in = True
                 st.session_state.full_name = full_name
                 URL_TO_GO = "https://bobs-list.streamlit.app?logged_in=true&password="+ hash_password(password)
