f16f43df1557401c8e8b4ce29ec38df167ced1ba
Sobhan Hota <gupuldw@yahoo.com>
2025-05-08 14:12:00 -0400

    update

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -75,7 +75,7 @@ hide_menu_style = """
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
-tab = st.radio("Choose:", ["Login", "Register(New Here)"])
+tab = st.radio("Choose:", ["Login", "Register (i.e for new contractor)"])
 
 if tab == "Login":
     #st.subheader("Contractor  to login:")
