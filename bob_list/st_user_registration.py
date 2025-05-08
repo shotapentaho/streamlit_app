1d2473b6180278624b111bafa440ff8959ac5048
Sobhan Hota <gupuldw@yahoo.com>
2025-05-08 08:52:52 -0400

    fixed

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -102,7 +102,7 @@ else:
     with col_1:
         new_password = st.text_input("Choose a password:", type="password")
     
-    col_2, col_3, col_4, col_5, col_6 = st.columns([1,1,1,1])
+    col_2, col_3, col_4, col_5, col_6 = st.columns([1,1,1,1,1])
     with col_2:
         contracting_company_name = st.text_input("Company Name:")
     with col_3:
