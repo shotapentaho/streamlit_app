e900b2ce75c4b6f20eafa59df305705929b3b4fe
Sobhan Hota <gupuldw@yahoo.com>
2025-05-08 10:12:56 -0400

    updated

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -94,7 +94,7 @@ if tab == "Login":
                 URL_TO_GO = "https://bobs-list.streamlit.app?logged_in=true&password="+ hash_password(password)
                 st.markdown(f"""
                     <a href="{URL_TO_GO}" target="_target">
-                        <button style='font-size:30px;padding:10px 20px;margin-top:20px;'>Click and provide customer feeback..</button>
+                        <button style='font-size:30px;padding:10px 20px;margin-top:20px;'>Click to provide your customer feeback..</button>
                     </a> <a></a> <a></a> <a></a>
                     <a href="{URL_TO_GO}" target="_target">
                         <button style='font-size:30px;padding:10px 20px;margin-top:20px;'>View, search, analyze customer data.</button>
