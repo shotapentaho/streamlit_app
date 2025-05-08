d98ca35ea770db5ff25a57ab8705e9541f6e0300
Sobhan Hota <gupuldw@yahoo.com>
2025-05-08 14:33:21 -0400

    udpate with list box for state

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -122,7 +122,14 @@ else:
     with col_4:
         contracting_company_city= st.text_input("City:")
     with col_5:
-        contracting_company_state = st.text_input("State:")
+        #contracting_company_state = st.text_input("State:")
+        contracting_company_state = st.selectbox("State:", options=[
+            "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+            "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+            "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+            "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+            "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+        ])
     with col_6:
         contracting_company_zip = st.text_input("Zip:")
 
