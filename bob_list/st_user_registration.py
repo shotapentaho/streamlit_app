da08e4b535ac928ebdec3c92669b873634ce5075
Sobhan Hota <gupuldw@yahoo.com>
2025-05-08 08:42:39 -0400

    updated

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -104,8 +104,11 @@ else:
     
     col_2, col_3, col_4 = st.columns([1,1,1])
     with col_2:
-        contracting_company_name = st.text_input("Company Name-City-State:")
-    
+        contracting_company_name = st.text_input("Company Name:")
+    with col_3:
+        contracting_company_city = st.text_input("City/Town:")
+    with col_4:
+        contracting_company_state = st.text_input("State:")
     if st.button("Register"):
         ok, msg = register_user(new_username, new_password, contracting_company_name)
         if ok:
