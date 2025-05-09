b090fe72401a617e5e6cd2c7dda5bbee242df3ae
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 08:45:17 -0400

    update

--- bob_list/bobs-list.py
+++ bob_list/bobs-list.py
@@ -7,9 +7,9 @@ import st_contractor_view_analyze
 tabs = st.tabs(["Customer Feedback", "View Analyze Feedbacks"])
 
 with tabs[0]:
-    st.write("You are on the Feedback Form")
+    st.write("You are on the Feedback")
     st_contractor_feedback.render()
 
 with tabs[1]:
-    st.write("You are on the Feedback Analysis")
+    st.write("You are on the View Analysis")
     st_contractor_view_analyze.render()
