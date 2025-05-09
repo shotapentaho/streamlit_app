bbd382edfc758c71d6a6fe2132c0c68d84b464e8
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 13:12:25 -0400

    updated

--- bob_list/st_contractor_view_analyze.py
+++ bob_list/st_contractor_view_analyze.py
@@ -22,7 +22,7 @@ def render():
     client = OpenAI(api_key=st.secrets["openai"]["api_key"])  # Or use env var
 
     # Question Input (reset each time a new file is uploaded)
-    question = st.text_input("💬 Ask a question (i.e English): ex show all data? OR group by [field] etc."
+    question = st.text_input("💬 Ask a question (i.e English): ex show all data? OR group by [field] etc.."
                              #,value=st.session_state.question
                             ,placeholder="ex: show all data OR group by [state] OR Activity_Date",   # 👈 Greyed out tip text
                              )
