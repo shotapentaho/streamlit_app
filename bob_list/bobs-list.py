a6d123fd3f9d0d2cc9397742f5ffed7234a1b8ca
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 11:22:17 -0400

    update

--- bob_list/bobs-list.py
+++ bob_list/bobs-list.py
@@ -4,6 +4,15 @@ import st_contractor_view_analyze
 
 st.set_page_config(page_title="Customer Xperiences..", layout="wide")
 
+if len(st.query_params)> 1:
+     # Update session state based on the URL parameter
+    if st.query_params["logged_in"] == "true":
+        st.session_state.logged_in = True      
+else:
+    st.error("You must be logged in to access this page.")
+    st.stop()  # Stops the app execution here if the user is not logged in.
+    st.session_state.logged_in = False
+
 cxloop_tabs = st.tabs(["Customer Feedback", "View Analyze Feedbacks"])
 
 with cxloop_tabs[0]:
