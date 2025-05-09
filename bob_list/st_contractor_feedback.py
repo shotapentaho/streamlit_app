5f8f1ebbb94108743a16dbc408454f36890ee3a8
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 09:45:49 -0400

    update

--- bob_list/st_contractor_feedback.py
+++ bob_list/st_contractor_feedback.py
@@ -54,6 +54,7 @@ def get_contractor_companies(conn):
     query = """
     SELECT contractor_id, contractor_name as name
     FROM test.public.contractors 
+    WHERE contractor_name = (select full_name from test.public.users where hashed_password = %s", (hash_password_str,))
     ORDER BY name
     """
     cur = conn.cursor()
