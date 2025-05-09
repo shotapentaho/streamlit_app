8bbc8907cc439d5836d8672a459a67263ca1c565
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 09:02:04 -0400

    udpated

--- bob_list/bobs-list.py
+++ bob_list/bobs-list.py
@@ -7,10 +7,8 @@ import st_contractor_view_analyze
 cxloop_tabs = st.tabs(["Customer Feedback", "View Analyze Feedbacks"])
 
 with cxloop_tabs[0]:
-
     st_contractor_feedback.render()
 
 
 with cxloop_tabs[1]:
-
     st_contractor_view_analyze.render()
