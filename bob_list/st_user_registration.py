5a1d60a565e403a6139e3273f9726c4cdf03f1da
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 07:02:31 -0400

    updated

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -65,7 +65,7 @@ if "full_name" not in st.session_state:
 
 # UI
 st.set_page_config(layout="wide")
-st.title(" 🔐 👷 Contractor Xperience Loop.. Login or Register")
+st.title(" 🔐 👷 🔄 Contractor Xperiences.. Login or Register")
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
