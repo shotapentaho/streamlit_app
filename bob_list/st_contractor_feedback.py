b826882dc125232b12fbcdc86772cac9e9882c8a
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 10:18:09 -0400

    upate

--- bob_list/st_contractor_feedback.py
+++ bob_list/st_contractor_feedback.py
@@ -54,6 +54,7 @@ def authenticated_user(hash_password_str):
 
 user_exists = is_valid_user (st.query_params["password"])
 valid_user_name = authenticated_user (st.query_params["password"])
+st.write(f"Welcome {valid_user_name}")
 
 
 if (user_exists[0] == False):
