623dae26a7e3807d0d0333f1e217bcc9bd93367a
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 08:57:17 -0400

    update

--- bob_list/bobs-list.py
+++ bob_list/bobs-list.py
@@ -2,7 +2,7 @@ import streamlit as st
 import st_contractor_feedback as cx_feedback
 import st_contractor_view_analyze as cx_analyze
 
-st.set_page_config(page_title="Customer Experiences CXLoop", layout="wide")
+st.set_page_config(page_title="Customer Experiences...", layout="wide")
 
 cxloop_tabs = st.tabs(["Customer Feedback", "View Analyze Feedbacks"])
 
