983c91183fdc090e86c413e1c130fc1ff9ca9aad
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 17:57:09 -0400

    update

--- cxloop/st_user_registration.py
+++ cxloop/st_user_registration.py
@@ -155,11 +155,21 @@ else:
             st.warning(msg)
 
 
-tab = st.radio("Forgot:", ["User", "Password"])
-
-if tab == "User":   
-    st.subheader("Forgot your username?")
-    st.write("Please contact your administrator to retrieve your username.")
+# --- Corner Link ---
+st.markdown(
+    """
+    <div style='text-align: right;'>
+        <a href='#' onclick="document.getElementById('forgot-box').style.display='block'; return false;">🔐 Forgot Password?</a>
+    </div>
+    <div id='forgot-box' style='display:none; margin-top: 10px;'>
+        <form action="" method="post">
+            <input type="email" name="email" placeholder="Enter your email" required style="width: 100%; padding: 5px;" />
+            <input type="submit" value="Send Reset Link" style="margin-top: 5px;" />
+        </form>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # If logged in
 if st.session_state.logged_in:
