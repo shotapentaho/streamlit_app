cb080455c211ff4f2fc64ca8e322099ad555d198
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 08:59:13 -0400

    update

--- bob_list/st_user_registration.py
+++ bob_list/st_user_registration.py
@@ -64,7 +64,7 @@ if "full_name" not in st.session_state:
     st.session_state.full_name = ""
 
 # UI
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 st.title(" 🔐 👷 🔄 Contractor Xperiences.. login or register(new here)")
 
 hide_menu_style = """
