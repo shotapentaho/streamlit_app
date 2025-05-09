51ec0c8b5d1613006c01bec20397a4d29e9639c2
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 15:00:09 -0400

    update

--- cxloop/st_user_registration.py
+++ cxloop/st_user_registration.py
@@ -79,6 +79,7 @@ hide_header_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
         header {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_header_menu_style, unsafe_allow_html=True)
