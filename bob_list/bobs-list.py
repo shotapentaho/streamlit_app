0ed8c679c90d41598f103387aa5313188526ff75
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 08:48:38 -0400

    update

--- bob_list/bobs-list.py
+++ bob_list/bobs-list.py
@@ -8,8 +8,10 @@ tabs = st.tabs(["Customer Feedback", "View Analyze Feedbacks"])
 
 with tabs[0]:
     st.write("You are on the Feedback")
+    
     st_contractor_feedback.render()
 
 with tabs[1]:
     st.write("You are on the View Analysis")
+
     st_contractor_view_analyze.render()
