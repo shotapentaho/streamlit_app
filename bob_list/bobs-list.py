2739df99e75809767b42a20b69ba619fd3254321
Sobhan Hota <gupuldw@yahoo.com>
2025-05-09 11:40:31 -0400

    update

--- bob_list/bobs-list.py
+++ bob_list/bobs-list.py
@@ -14,36 +14,6 @@ else:
     st.session_state.logged_in = False
 
 
-# Connect to Snowflake using Streamlit secrets
-def get_connection():
-    return snowflake.connector.connect(
-        user=st.secrets["snowflake"]["user"],
-        password=st.secrets["snowflake"]["password"],
-        account=st.secrets["snowflake"]["account"],
-        warehouse=st.secrets["snowflake"]["warehouse"],
-        database=st.secrets["snowflake"]["database"],
-        schema=st.secrets["snowflake"]["schema"]
-    )
-
-# Check password received in URL
-def is_valid_user(user_str):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT username FROM TEST.PUBLIC.users WHERE username = %s", (user_str,))
-    row = cur.fetchone()
-    if not row:
-        return False, None
-    else:
-        username = row
-        return True, username
-    return False, None
-
-user_exists = is_valid_user (st.query_params["username"])
-if (user_exists[0] == False):
-    st.error("You must be logged in to access this page.")
-    st.stop()  # Stops the app execution here if the user is not logged in.
-    st.session_state.logged_in = False
-
 cxloop_tabs = st.tabs(["Customer Feedback", "View Analyze Feedbacks"])
 
 with cxloop_tabs[0]:
