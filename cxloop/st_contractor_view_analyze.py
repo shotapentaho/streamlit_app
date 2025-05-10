791074f61d869f83e90745947f62e2dea7439c21
Sobhan Hota <gupuldw@yahoo.com>
2025-05-10 14:47:30 -0400

    updated

--- cxloop/st_contractor_view_analyze.py
+++ cxloop/st_contractor_view_analyze.py
@@ -38,7 +38,7 @@ def render():
     df = cur.fetch_pandas_all()
     st.session_state.df = df
     # Display the dataframe preview
-    st.write("✅ Data Preview", df.head())
+    st.write("✅ All Data ", df)
     #st.dataframe(df.head(), use_container_width=True)
 
     if question:
