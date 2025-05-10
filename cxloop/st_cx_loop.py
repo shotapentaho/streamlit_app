fb97a51c5d9158ac5577f3e89ad9975d9be17f45
Sobhan Hota <gupuldw@yahoo.com>
2025-05-10 11:21:20 -0400

    updated

--- cxloop/st_cx_loop.py
+++ cxloop/st_cx_loop.py
@@ -15,7 +15,7 @@ else:
     st.session_state.logged_in = False
 
 
-cxloop_tabs = st.tabs(["Feedback Entry", "View, filter, analyze data"])
+cxloop_tabs = st.tabs(["Feedback Entry", "View Filter Analyze Entries"])
 
 with cxloop_tabs[0]:
     st_contractor_feedback.render()
