988a871c3a8a507fe4e3a4e9cc3bf75d18527086
Sobhan Hota <gupuldw@yahoo.com>
2025-05-10 08:17:50 -0400

    updated

--- cxloop/st_contractor_view_analyze.py
+++ cxloop/st_contractor_view_analyze.py
@@ -55,8 +55,8 @@ def render():
                         - JOIN engagements.contractor_id = contractors.contractor_id
 
                         Goal:
+                        - Ignore all columns contractors table except contractor_name.
                         - Return only relevant columns needed from engagements tables.
-                        - Ignore all columns contractors table except contractor_name. 
 
                         Schema: {st.session_state.df.dtypes.astype(str).to_string()}
                         Question: {question}
