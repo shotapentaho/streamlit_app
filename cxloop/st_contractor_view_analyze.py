064c39577ba2f91aad8d8f9039a1e1db7eda1a68
Sobhan Hota <gupuldw@yahoo.com>
2025-05-11 18:17:15 -0400

    udpated

--- cxloop/st_contractor_view_analyze.py
+++ cxloop/st_contractor_view_analyze.py
@@ -23,7 +23,7 @@ def render():
     client = OpenAI(api_key=st.secrets["openai"]["api_key"])  # Or use env var
 
     # Question Input (reset each time a new file is uploaded)
-    question = st.text_input("💬 Ask your questions here (i.e English),   Ex: show data OR filter a State OR group by State etc.."
+    question = st.text_input("💬 Query here (i.e. in English),   Ex: show data OR filter a State OR group by State etc.."
                              #,value=st.session_state.question
                             ,placeholder="ex: show all data OR group by [state] OR Activity_Date",   # 👈 Greyed out tip text
                              )
