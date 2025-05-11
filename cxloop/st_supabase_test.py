3a89dc3349d7f27b2c649064a799c1d6c6d32c33
Sobhan Hota <gupuldw@yahoo.com>
2025-05-11 09:50:37 -0400

    updated

--- cxloop/st_supabase_test.py
+++ cxloop/st_supabase_test.py
@@ -1,5 +1,6 @@
 from supabase import create_client
 import streamlit as st
+import pandas as pd
 
 sb_url = st.secrets["supabase"]["url"]
 anon_key = st.secrets["supabase"]["key"]
@@ -8,4 +9,8 @@ supabase = create_client(sb_url, anon_key)
 # Fetch data from a table
 response = supabase.table("users").select("*").execute()
 
-st.write(response.data)
+# Convert to DataFrame
+df = pd.DataFrame(response.data)
+
+# Display using Streamlit
+st.dataframe(df)
