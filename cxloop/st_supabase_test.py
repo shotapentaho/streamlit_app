7e23bd5217abc0dc4377db11debfe5d508713b6b
Sobhan Hota <gupuldw@yahoo.com>
2025-05-11 09:01:24 -0400

    enter

--- cxloop/st_supabase_test.py
+++ cxloop/st_supabase_test.py
@@ -6,11 +6,11 @@ import pandas as pd
 def get_connection():
     # Connect to Supabase PostgreSQL using the secrets
     return psycopg2.connect(
-        host=st.secrets["supabase"]["host"],
-        database=st.secrets["supabase"]["database"],
-        port=st.secrets["supabase"]["port"],
         user=st.secrets["supabase"]["user"],
         password=st.secrets["supabase"]["password"]
+        host=st.secrets["supabase"]["host"],
+        port=st.secrets["supabase"]["port"],
+        database=st.secrets["supabase"]["database"]
     )
 
 # Establish connection
