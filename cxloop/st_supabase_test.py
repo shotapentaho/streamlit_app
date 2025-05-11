d4e7ff4b88b04d6245923148482c19f1c7f0bad7
Sobhan Hota <gupuldw@yahoo.com>
2025-05-11 08:42:37 -0400

    updated

--- cxloop/st_supabase_test.py
+++ cxloop/st_supabase_test.py
@@ -2,22 +2,19 @@ import streamlit as st
 import psycopg2
 import pandas as pd
 
-# Load credentials from secrets.toml
-config = st.secrets["supabase"]
 
-@st.cache_resource
-def connect():
+def get_connection():
     # Connect to Supabase PostgreSQL using the secrets
     return psycopg2.connect(
-        host=config["host"],
-        dbname=config["database"],
-        user=config["user"],
-        password=config["password"],
-        port=config["port"]
+        host=st.secrets["supabase"]["host"],
+        database=st.secrets["supabase"]["database"],
+        port=st.secrets["supabase"]["port"],
+        user=st.secrets["supabase"]["user"],
+        password=st.secrets["supabase"]["password"]
     )
 
 # Establish connection
-conn = connect()
+conn = get_connection()
 
 # Query a table (e.g., contractors)
 df = pd.read_sql("SELECT * FROM cxloop.users", conn)
