49c952b6b389894ae5c864b43d296e5c9e24a8fd
Sobhan Hota <gupuldw@yahoo.com>
2025-05-11 09:01:43 -0400

    update

--- cxloop/st_supabase_test.py
+++ cxloop/st_supabase_test.py
@@ -7,7 +7,7 @@ def get_connection():
     # Connect to Supabase PostgreSQL using the secrets
     return psycopg2.connect(
         user=st.secrets["supabase"]["user"],
-        password=st.secrets["supabase"]["password"]
+        password=st.secrets["supabase"]["password"],
         host=st.secrets["supabase"]["host"],
         port=st.secrets["supabase"]["port"],
         database=st.secrets["supabase"]["database"]
