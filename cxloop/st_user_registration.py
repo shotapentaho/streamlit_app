9b007b9cdfac8ae3f73b700f169abf0bd3c36b4d
Sobhan Hota <gupuldw@yahoo.com>
2025-05-11 10:24:49 -0400

    updated

--- cxloop/st_user_registration.py
+++ cxloop/st_user_registration.py
@@ -28,6 +28,7 @@ import os
 # Hash passwords securely
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
+
 def get_connection():
     return snowflake.connector.connect(
         user=st.secrets["snowflake"]["user"],
