18d578febb7292008407667b5cff36600de088b1
Sobhan Hota <gupuldw@yahoo.com>
2025-05-12 09:12:23 -0400

    udopated

--- cxloop/st_home_page.py
+++ cxloop/st_home_page.py
@@ -4,3 +4,11 @@ from PIL import Image
 # Load and show logo
 logo = Image.open("./cxloop/media/cxloop_logo.png")
 st.image(logo, use_container_width=True)
+
+# Title
+st.markdown("## 🔐 👷 🔄 Contractor Xperiences")
+st.markdown("### Login or Register (new here)")
+
+# Navigation button
+if st.button("🔑 Go to Login/Register Page"):
+    st.switch_page("https://cxloop-enter.streamlit.app/")  # or replace with actual filename
