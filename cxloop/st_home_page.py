73ec0d24726e5d68e476fabb469be4407dd4aacb
Sobhan Hota <gupuldw@yahoo.com>
2025-05-12 09:06:55 -0400

    path updated

--- cxloop/st_home_page.py
+++ cxloop/st_home_page.py
@@ -2,5 +2,5 @@ import streamlit as st
 from PIL import Image
 
 # Load and show logo
-logo = Image.open("./media/logo.png")
+logo = Image.open('./cxloop/media/logo.png')
 st.image(logo, use_column_width=True)
