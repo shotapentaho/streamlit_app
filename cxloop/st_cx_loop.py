0e22418d57b1280ed665ec3c74093836b82e48ec
Sobhan Hota <gupuldw@yahoo.com>
2025-05-13 15:13:01 -0400

    uodated

--- cxloop/st_cx_loop.py
+++ cxloop/st_cx_loop.py
@@ -42,9 +42,14 @@ else:
     st.error("You must be logged in to access this page.")
     st.stop()  # Stops the app execution here if the user is not logged in.
 
-if "logout_triggered" not in st.session_state:
-    st.session_state.logout_triggered = False
 
+# --- TOP RIGHT LOGOUT BUTTON ---
+top_col1, top_col2 = st.columns([8, 1])
+with top_col2:
+    st.session_state.logged_in = False
+    st.session_state.logout_triggered = True
+    st.markdown("[Logout..](https://cxloop.co/)")
+    st.stop()
 
 # --- Main content if still logged in ---
 if st.session_state.get("logged_in"):
@@ -57,10 +62,3 @@ if st.session_state.get("logged_in"):
         st_contractor_view_analyze.render()
 
 
-# --- TOP RIGHT LOGOUT BUTTON ---
-top_col1, top_col2 = st.columns([8, 1])
-with top_col2:
-    st.session_state.logged_in = False
-    st.session_state.logout_triggered = True
-    st.markdown("[Logout..](https://cxloop.co/)")
-    st.stop()
