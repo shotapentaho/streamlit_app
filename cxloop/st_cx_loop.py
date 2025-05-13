421d7ae3db5dd3daedc3d56de3f208061237a753
Sobhan Hota <gupuldw@yahoo.com>
2025-05-13 15:14:27 -0400

    updated

--- cxloop/st_cx_loop.py
+++ cxloop/st_cx_loop.py
@@ -43,14 +43,6 @@ else:
     st.stop()  # Stops the app execution here if the user is not logged in.
 
 
-# --- TOP RIGHT LOGOUT BUTTON ---
-top_col1, top_col2 = st.columns([8, 1])
-with top_col2:
-    st.session_state.logged_in = False
-    st.session_state.logout_triggered = True
-    st.markdown("[Logout..](https://cxloop.co/)")
-    st.stop()
-
 # --- Main content if still logged in ---
 if st.session_state.get("logged_in"):
     cxloop_tabs = st.tabs(["Feedback Entry", "Query all feedbacks "])
