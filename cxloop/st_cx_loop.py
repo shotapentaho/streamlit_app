9e44956437cf31ee6ea1dc5b7352a2c1c1c2f682
Sobhan Hota <gupuldw@yahoo.com>
2025-05-13 13:26:33 -0400

    udpated typo

--- cxloop/st_cx_loop.py
+++ cxloop/st_cx_loop.py
@@ -23,7 +23,7 @@ with top_col2:
         # Inject JavaScript redirect
         st.markdown("""
             <script>
-                window.location.href = "https://cxloop.io";
+                window.location.href = "https://cxloop.co";
             </script>
         """, unsafe_allow_html=True)
         st.stop()
