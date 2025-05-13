301197dce30395c2106f347a38587d4c976d0ea0
Sobhan Hota <gupuldw@yahoo.com>
2025-05-13 13:57:46 -0400

    updated

--- cxloop/st_cx_loop.py
+++ cxloop/st_cx_loop.py
@@ -46,12 +46,12 @@ else:
 top_col1, top_col2 = st.columns([8, 1])
 with top_col2:
     if st.button("Logout"):
+        URL_TO_LOGOUT = "https://cxloop-co.streamlit.app/"
         st.session_state.logged_in = False
-            # JavaScript to open cxloop.io in a new tab
-        st.markdown("""
-            <script>
-                window.open("https://cxloop.co", "_blank");
-            </script>
+        st.markdown(f"""
+        <a href="{URL_TO_LOGOUT}" target="_target">
+        <button style='font-size:30px;padding:10px 20px;margin-top:20px;'>Logout..</button>
+        </a> 
         """, unsafe_allow_html=True)
 
         st.stop()
