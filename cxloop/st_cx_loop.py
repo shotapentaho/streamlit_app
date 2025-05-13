a8640578a08b639b43d61cbd34b38e622a0e3c94
Sobhan Hota <gupuldw@yahoo.com>
2025-05-13 13:50:38 -0400

    updated

--- cxloop/st_cx_loop.py
+++ cxloop/st_cx_loop.py
@@ -1,9 +1,37 @@
-
+hide_default_header = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        header {visibility: hidden;}
+        footer {visibility: hidden;}
+        </style>
+        """
+hide_default_footer = """
+    <style>
+    footer, .st-emotion-cache-1gulkj5 {display: none; visibility: hidden;}
+    .css-qri22k {display: none; visibility: hidden;}
+    .stDeployButton {display: none;}
+    .viewerBadge_link__1S137 {display: none;}
+    </style>
+"""
+hide_github_icon = """
+    <style>
+        .viewerBadge_container__1QSob,
+        .viewerBadge_link__1S137,
+        .viewerBadge_text__1JaDK,
+        #MainMenu,
+        footer {
+            display: none !important;
+        }
+    </style>
+"""
 import streamlit as st
+st.set_page_config(page_title="Customer Xperiences..", layout="wide")
+st.markdown(hide_default_footer, unsafe_allow_html=True)
+st.markdown(hide_default_header, unsafe_allow_html=True)
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 import st_contractor_feedback
 import st_contractor_view_analyze
 
-st.set_page_config(page_title="Customer Xperiences..", layout="wide")
 
 if len(st.query_params)> 1:
      # Update session state based on the URL parameter
