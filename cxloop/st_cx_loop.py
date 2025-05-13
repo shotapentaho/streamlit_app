120bf3096553e87c507d8ae3715310861955daef
Sobhan Hota <gupuldw@yahoo.com>
2025-05-13 14:00:59 -0400

    updated

--- cxloop/st_cx_loop.py
+++ cxloop/st_cx_loop.py
@@ -45,7 +45,7 @@ else:
 # --- TOP RIGHT LOGOUT BUTTON ---
 top_col1, top_col2 = st.columns([8, 1])
 with top_col2:
-    if st.button("Logouttt"):
+    if st.button("Logout"):
         st.session_state.logged_in = False
         st.stop()
 
