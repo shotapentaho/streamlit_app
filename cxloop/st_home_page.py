e9cd42df50fdb61145642d6f87db824dff4a45ca
Sobhan Hota <gupuldw@yahoo.com>
2025-05-14 13:45:48 -0400

    updated

--- cxloop/st_home_page.py
+++ cxloop/st_home_page.py
@@ -44,7 +44,7 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 # Create two columns: left wide, right narrow (logo)
-col1, col2 = st.columns([2, 20])
-with col1:
+col1, col2 = st.columns([20, 2])
+with col2:
     st.image(brain_python_openai_logo, use_container_width=True)
 #with col2:
