332401e6ea37a0b8194e37212b5f3d2ec7c1b319
Sobhan Hota <gupuldw@yahoo.com>
2025-05-14 13:42:10 -0400

    updated

--- cxloop/st_home_page.py
+++ cxloop/st_home_page.py
@@ -24,8 +24,10 @@ brain_python_openai_logo = Image.open("./cxloop/media/human_python_openai.png")
 #logo = Image.open("./cxloop/media/contractors_at_job.jpeg")
 # Create two columns: left wide, right narrow (logo)
 col1, col2 = st.columns([8, 3])
-with col2:
+with col1:
     st.image(brain_python_openai_logo, use_container_width=True)
+with col2:
+    
 
 st.image(cxloop_logo, use_container_width=True)
 
