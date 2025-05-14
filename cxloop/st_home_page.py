30249e48f63bffb5afd9cb35a0b568551c359a66
Sobhan Hota <gupuldw@yahoo.com>
2025-05-14 13:25:45 -0400

    updated

--- cxloop/st_home_page.py
+++ cxloop/st_home_page.py
@@ -22,13 +22,13 @@ from PIL import Image
 cxloop_logo = Image.open("./cxloop/media/cxloop_logo.png")
 brain_python_openai_logo = Image.open("./cxloop/media/human_python_openai.png")
 #logo = Image.open("./cxloop/media/contractors_at_job.jpeg")
-
-st.image(cxloop_logo, use_container_width=True)
-
 # Create two columns: left wide, right narrow (logo)
 col1, col2 = st.columns([9, 1])
 with col2:
     st.image(brain_python_openai_logo, use_column_width=True)
+
+st.image(cxloop_logo, use_container_width=True)
+
 
 # Top-right button with CSS
 st.markdown("""
