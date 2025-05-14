1c3fb996ab667b74e154413ab30284639516b34f
Sobhan Hota <gupuldw@yahoo.com>
2025-05-14 13:46:56 -0400

    just a blank row

--- cxloop/st_home_page.py
+++ cxloop/st_home_page.py
@@ -44,6 +44,7 @@ st.markdown("""
     </div>
 """, unsafe_allow_html=True)
 # Create two columns: left wide, right narrow (logo)
+st.write
 col1, col2 = st.columns([20, 4])
 with col2:
     st.image(brain_python_openai_logo, use_container_width=True)
