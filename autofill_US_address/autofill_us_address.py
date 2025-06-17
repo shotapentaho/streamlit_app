e981d25cad0d79ba4f966c1ff8a6d2ef6af70d6d
Sobhan Hota <gupuldw@yahoo.com>
2025-06-16 20:34:24 -0400

    updated

--- autofill_US_address/autofill_us_address.py
+++ autofill_US_address/autofill_us_address.py
@@ -19,7 +19,7 @@ def autocomplete_address(input_text):
 def get_place_details(place_id):
     url = (
         f"https://maps.googleapis.com/maps/api/place/details/json"
-        f"?place_id={place_id}&fields=address_components&key={GOOGLE_API_KEY}"
+        f"?place_id={place_id}&fields=address_components,formatted_address&key={GOOGLE_API_KEY}"
     )
     r = requests.get(url)
     if r.status_code == 200:
@@ -30,20 +30,21 @@ def get_place_details(place_id):
 def extract_address_components(components):
     city = state = zip_code = ""
     for comp in components:
-        if "locality" in comp["types"] or "postal_town" in comp["types"]:
+        if "locality" in comp["types"]:
+            city = comp["long_name"]
+        elif "postal_town" in comp["types"] and not city:
+            city = comp["long_name"]
+        elif "sublocality" in comp["types"] and not city:
+            city = comp["long_name"]
+        elif "administrative_area_level_2" in comp["types"] and not city:
             city = comp["long_name"]
         elif "administrative_area_level_1" in comp["types"]:
             state = comp["short_name"]
         elif "postal_code" in comp["types"]:
             zip_code = comp["long_name"]
-        # Fallback for city
-        elif "sublocality" in comp["types"] and not city:
-            city = comp["long_name"]
-        elif "administrative_area_level_2" in comp["types"] and not city:
-            city = comp["long_name"]
     return city, state, zip_code
 
-st.title("US Address Autocomplete & Autofill")
+st.title("US Address Autocomplete & Autofill (with Debug)")
 
 street_input = st.text_input("Start typing street address...")
 
@@ -63,6 +64,7 @@ if "autofill_data" not in st.session_state:
 
 if selected_place_id:
     details = get_place_details(selected_place_id)
+    st.write("Google Place Details:", details)  # <--- DEBUG! See what comes back
     components = details.get("address_components", [])
     city, state, zip_code = extract_address_components(components)
     st.session_state["autofill_data"] = {
