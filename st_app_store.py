71e56e2d90e9557014bf468cf0886368d8ef7bcc
Sobhan Hota <gupuldw@yahoo.com>
2025-06-18 09:09:40 -0400

    updated

--- st_app_store.py
+++ st_app_store.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Hot-Store", layout="wide")
 
 # Streamlit app title
-st.title("🔥 Hot apps @ hot-store 🚀")
+st.title("🔥 Hot apps @ Hot-Store 🚀")
 
 
 # Dictionary of Streamlit URLs (Replace with your actual links)
