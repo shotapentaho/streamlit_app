ff870941ad098bae647819d4193b54559e673ee5
Sobhan Hota <gupuldw@yahoo.com>
2025-06-18 16:26:58 -0400

    udpayed

--- weather/st_open_weather.py
+++ weather/st_open_weather.py
@@ -142,7 +142,7 @@ if st.session_state.get('get_aqi', False):
 
     data = get_weather_forecast(city)
     coord = data['city']['coord']
-    st.write(data=pd.DataFrame([{
+    st.map(data=pd.DataFrame([{
             'lat': coord['lat'],
             'lon': coord['lon']
         }]))
